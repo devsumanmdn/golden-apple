07266fed8412b89e06c4c5e39a7bea9016757542
devsumanmdn <devsumanmdn@gmail.com>
2017-11-12 11:46:21 +0530

    added package-lock.json to .gitignore and changed connnect to createConnection in config/config.js for deprecation error

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const keys = require('./keys');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(keys.MONGODB_URI, { useMongoClient: true });
+mongoose.createConnection(keys.MONGODB_URI, { useMongoClient: true });
 
 module.exports = { mongoose };
