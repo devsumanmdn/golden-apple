9c917c1412a420490508f79a1610fe2b9d202eec
Mohammmad Aziz <mdaziz067@gmail.com>
2017-11-16 14:35:40 +0530

    Add react component to handle signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errorMessage: '',
+      successMessage: ''
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    axios
+      .post('/api/users/signup', {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => {
+        this.setState({
+          successMessage: 'Hurray, user created!',
+          errorMessage: ''
+        });
+      })
+      .catch(e => {
+        this.setState({
+          errorMessage: 'Sorry, invalid user data!',
+          successMessage: ''
+        });
+      });
+    this.setState({ email: '', password: '' });
+  }
+
+  handleInputChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" value="Signup" />
+        </form>
+        {this.state.errorMessage ? <p>{this.state.errorMessage}</p> : ''}
+        {this.state.successMessage ? <p>{this.state.successMessage}</p> : ''}
       </div>
     );
   }
