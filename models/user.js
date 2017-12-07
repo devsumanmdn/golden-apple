98af7478c3fbcdc5670a31aea573d61bd9bc7167
Mohammmad Aziz <mdaziz067@gmail.com>
2017-12-07 18:12:07 +0530

    Fix bug of undefined data when wrong credentials are passed

--- models/user.js
+++ models/user.js
@@ -88,20 +88,22 @@ UserSchema.pre('save', function(next) {
 
 UserSchema.statics.findByCredentials = function(prop, value, password) {
   const User = this
-  return User.findOne({ [prop]: value }).then(user => {
-    if (!user) {
-      return Promise.reject()
-    }
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          resolve(user)
-        } else {
-          reject()
-        }
+  return User.findOne({ [prop]: value })
+    .then(user => {
+      if (!user) {
+        return Promise.reject()
+      }
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user)
+          } else {
+            reject()
+          }
+        })
       })
     })
-  })
+    .catch(e => e)
 }
 
 const User = mongoose.model('user', UserSchema)
