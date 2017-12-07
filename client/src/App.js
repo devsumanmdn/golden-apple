33acb04dd2a3d373e4696a423072aadb6def97ab
Mohammmad Aziz <mdaziz067@gmail.com>
2017-12-07 18:09:50 +0530

    Add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import SignupForm from './signup';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import { BrowserRouter, Route } from 'react-router-dom'
+// import './styles/App.css'
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-  }  render() {
+    super(props)
+    this.state = {}
+  }
+  render() {
     return (
       <div>
-        <header>
-          <img src={logo} alt="My logo" width="100" height="100" />
-        </header>
-        <SignupForm />
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </div>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
