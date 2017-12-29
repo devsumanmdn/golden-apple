904f636f211baa9e6dce44959b108ed6222b6bf4
Mohammmad Aziz <mdaziz067@gmail.com>
2017-12-29 20:49:04 +0530

    Add redux with reducers and action creators

---
+++ client/src/actions/index.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from './types'
+
+export function signinUser(uid, password) {
+  // eslint-disable-next-line
+  return function(dispatch) {
+    axios
+      .post('/api/users/login', { uid, password })
+      .then(res => {
+        if (res.data.token) {
+          dispatch({ type: AUTH_USER })
+          localStorage.setItem('auth_token', res.data.token)
+        }
+      })
+      .catch(e => {
+        dispatch(authError(e.response.statusText))
+      })
+  }
+}
+
+export function signupUser(email, username, password) {
+  // eslint-disable-next-line
+  return function(dispatch) {
+    axios
+      .post('/api/users/signup', { email, password, username })
+      .then(res => {
+        if (res.data.token) {
+          dispatch({ type: AUTH_USER })
+          localStorage.setItem('auth_token', res.data.token)
+        }
+      })
+      .catch(e => {
+        dispatch(authError(e.response.statusText))
+      })
+  }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('auth_token')
+  return { type: UNAUTH_USER }
+}
+
+function authError(error) {
+  return { type: AUTH_ERROR, payload: error }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import App from './components/App'
+import reducers from './reducers'
+import { AUTH_USER } from './actions/types'
 import registerServiceWorker from './registerServiceWorker'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+const token = localStorage.getItem('auth_token')
+if (token) {
+  store.dispatch({ type: AUTH_USER })
+}
+
+store.getState()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
