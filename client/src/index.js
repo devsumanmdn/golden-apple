c480abfebb77c23ed5845c0fb7af298f150336f8
Mohammmad Aziz <mdaziz067@gmail.com>
2018-01-26 15:52:37 +0530

    Fix loading issue in header

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,21 @@ import axios from 'axios'
 import { createStore, applyMiddleware } from 'redux'
 import App from './components/App'
 import reducers from './reducers'
-import { AUTH_USER, UNAUTH_USER } from './actions/types'
+import { AUTH_USER, UNAUTH_USER, FETCH_USER } from './actions/types'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
+// Show blank header while user is fetching
+store.dispatch({ type: FETCH_USER })
+
 axios
   .get('/api/current_user')
   .then(res => {
     if (res.data) {
       store.dispatch({ type: AUTH_USER, payload: res.data.username })
+    } else {
+      store.dispatch({ type: UNAUTH_USER })
     }
   })
   // eslint-disable-next-line no-unused-vars
